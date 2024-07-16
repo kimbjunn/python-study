a23fd4dc89d3f5e562d92985aa8157090d052c5e
unknown <ksr9852@dgu.ac.kr>
2024-07-16 13:37:31 +0900

    Add Sungkyunkwan University notice board crawler with scrapy

---
+++ skku_spider.py
@@ -0,0 +1,31 @@
+import scrapy
+
+
+class SkkuSpiderSpider(scrapy.Spider):
+    name = "skku_spider"
+    allowed_domains = ["forensic.skku.edu"]
+    start_urls = ["https://forensic.skku.edu/forensic/community/grad_notice.do"]
+
+    def parse(self, response):
+        articles = []
+        for article in response.css("ul.board-list-wrap > li"):
+            data={
+                "title": article.css("dl > dt a::text").get().replace("\n","").replace("\t","").replace(" ",""),#함수로 만들면 더 쉬움
+                "link": article.css("dl > dt a::attr(href)").get()
+            }   
+            articles.append(data)
+
+            for page in response.css('ul.paging-wrap li'):
+                link = page.css('a::attr(href)').get()
+                link_text = page.css('a::text').get()
+
+                if link_text:
+                    link_text = link_text.strip()
+                
+                if link_text and link_text.isdigit() and int(link_text) <= 3:
+                    yield { 'page_number': link_text, 'link': link }
+                    yield response.follow(link, self.parse)
+        print(articles)
+
+
+            
